c44ee580c0ef8c48a65dc428c9ed7ee3db1b4634
thiagoinovmi <thiago.inovmi@gmail.com>
2025-11-21 15:31:21 +0000

    style: Aumentar tamanho do layout de col-xl-5 para col-xl-6
    - Aumentar largura do container de autenticação
    - Melhor aproveitamento do espaço em telas grandes
    - col-xl-5 → col-xl-6 para melhor responsividade

--- eventmie-pro/resources/views/auth/authapp.blade.php
+++ eventmie-pro/resources/views/auth/authapp.blade.php
@@ -10,7 +10,7 @@
                         <div class="min-vh-100 py-md-10 py-lg-0">
                             <div class="container">
                                 <div class="row d-flex align-items-center min-vh-100 justify-content-center">
-                                    <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12 py-10">
+                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 py-10">
                                         <!--lgx-registration-banner-box-->
                                         @yield('authcontent')
                                     </div>
