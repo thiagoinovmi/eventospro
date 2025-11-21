a4dd3e95133a6a3abcd63b266f2758d41146393f
thiagoinovmi <thiago.inovmi@gmail.com>
2025-11-21 20:41:30 +0000

    fix: Adicionar @yield para botão salvar em formulários de Kits
    - Adicionar @yield('submit-buttons') em kits/edit-add.blade.php
    - Adicionar @yield('submit-buttons') em kit_items/edit-add.blade.php
    - Seguir padrão de outros formulários (categories, events, etc)
    - Botão salvar agora aparece corretamente

--- eventmie-pro/resources/views/vendor/voyager/kit_items/edit-add.blade.php
+++ eventmie-pro/resources/views/vendor/voyager/kit_items/edit-add.blade.php
@@ -98,6 +98,7 @@
                             @section('submit-buttons')
                                 <button type="submit" class="btn btn-primary save">{{ __('voyager::generic.save') }}</button>
                             @stop
+                            @yield('submit-buttons')
                         </div>
                     </form>
 
