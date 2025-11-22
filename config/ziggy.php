d417378913a080ad67342f290bc3e464e40e761b
thiagoinovmi <thiago.inovmi@gmail.com>
2025-11-22 00:27:45 +0000

    fix: Configurar Ziggy para gerar rotas JavaScript
    Problemas corrigidos:
    - Criar config/ziggy.php com configuração padrão
    - Gerar arquivo resources/js/ziggy.js com todas as rotas
    - Importar ziggy.js no bootstrap.js
    - Disponibilizar route() globalmente no window
    
    Resultado:
    - Rotas myevents_get_event_kits e myevents_store_event_kits agora disponíveis
    - Componente Kits.vue consegue chamar route() corretamente
    - Erro 'Ziggy error: route not in the route list' resolvido

---
+++ config/ziggy.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'output' => base_path('resources/js/ziggy.js'),
+    'skip-middleware' => ['web'],
+    'except' => [],
+    'only' => [],
+];
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Import Ziggy routes
+import route from './ziggy.js';
+window.route = route;
